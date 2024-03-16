355cdbe984fbd80789706f1e6503bddf6ed4ead9
Tika <162357343+MadameTiks@users.noreply.github.com>
2024-03-16 02:49:56 -0400

    HomeWork#9

--- third.js
+++ third.js
@@ -1,27 +1,24 @@
 
 /* 1.დაწერეთ ფუნქცია რომელსაც გადასცემ ორ რიცხვს (a,b) და დააბრუნებს შესაბამისს ტექსტს 
-a)თუ rიცხვები ტოლია : a  და b ტოლია
-b)თუ a მეტია b ze : a  მეტია b -ზე
-c)თუ a ნაკლებია  b ze : a  ნაკლებია  b -ზე
-*/
+a)თუ rიცხვები ტოლია : a და b ტოლია
+b)თუ a მეტია b ze : a მეტია b_ზე
+c)თუ a ნაკლებია  b ze : a ნაკლებია b_ზე  */
 
 let a=23, b= 123;
 if (a==b) {
-    console.log(a=b);
+    console.log("a და b ტოლია");
 }
 else if (a>b) {
-    console.log(a>b);
+    console.log("a მეტია b_ზე");
 }
 else if (a<b) {
-    console.log(a<b);
+    console.log("a ნაკლებია b_ზე");
 }
 
 
 
-
-/* 2.დაწერეთ ფუნქცია რომელსაც გადასცემთ ორ ცვლადს(რიცხვს), 
-დააბრუნეთ true თუ მათი ჯამი მეტია 100 ზე , false , თუ არ არის მეტი 100 ზე 
-*/
+/* 2.დაწერეთ ფუნქცია რომელსაც გადასცემთ ორ ცვლადს(რიცხვს),
+დააბრუნეთ true თუ მათი ჯამი მეტია 100 ზე , false , თუ არ არის მეტი 100 ზე   */
 
 function calculatenums(num1, num2) {
 
@@ -32,28 +29,39 @@ console.log(calculatenums(55, 69));
 console.log(calculatenums(33, 37));
 
 
-/* 3.დაწერეთ ფუნქცია, რომელიც დაითვლის და დააბრუნებს კალათბურთის თამაშისას აღებულ ქულას ( ქულების ჯამს).
- ფუნქციას აქვს სამი პარამეტრი
-a)პირველი პარამეტრი - 1 ქულიანების რაოდენობა
-b)მეორე პარამეტრი -  2 ქულიანების რაოდენობა
-c)მესამე - 3 ქულიანების რაოდენობა 
-*/
 
-function BasketballGameScore(points1, points2, points3) {
+/* 3.დაწერეთ ფუნქცია, რომელიც დაითვლის და დააბრუნებს კალათბურთის თამაშისას აღებულ ქულას ( ქულების ჯამს).
+ფუნქციას აქვს სამი პარამეტრი a)პირველი პარამეტრი - 1 ქულიანების რაოდენობა b)მეორე პარამეტრი -  2 ქულიანების რაოდენობა c)მესამე - 3 ქულიანების რაოდენობა   */
+
+function BasketballGamesum(points1, points2, points3) {
 
     return points1 + points2 + points3;
 }
-console.log(BasketballGameScore(7, 22, 123));
+console.log(BasketballGamesum(7, 22, 123));
 
 
-BasketballGameScore ()
+BasketballGamesum ()
 
-/* 4.დაწერეთ ფუნქცია ორი პარამეტრით start და end , ფუნქციას გადაეცემა ორი რიცხვი, 
-უნდა შექმნათ  start დან end-ჩათვლით რიცხვების მასივი
-(მაგალითად თუ start არის 3 და end არის 6 
-მასივის ელემენტები უნდა იყოს [3,4,5,6] ) ( arr.push()- მასივში ელემენტის დამატება)
-*/
 
+
+
+/* 4.დაწერეთ ფუნქცია ორი პარამეტრით start და end , ფუნქციას გადაეცემა ორი რიცხვი, უნდა შექმნათ  start დან end-ჩათვლით რიცხვების მასივი
+(მაგალითად თუ start არის 3 და end არის 6  მასივის ელემენტები უნდა იყოს [3,4,5,6] ) ( arr.push()- მასივში ელემენტის დამატება)   */
+
+function parameterArray(start, end) {
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+let start = 23, end = 78;
+
+let arr = parameterArray(start, end);
+
+
+console.log(arr); 
 
 
 
@@ -101,7 +109,9 @@ for ( ph of phones) {
 let num = 37;
 
 while (num <= 48) {
+
     console.log(num);
+
     num++;
 }
 
@@ -115,16 +125,11 @@ while (num <= 48) {
 let n = 2;
 
 for (n; n<=13; n++) {
-
-
     while ( n<=13) {
 
         console.log(n)
-    
-       n+=2
-}
- 
-  
+ n+=2
+          }
 }
 
    
